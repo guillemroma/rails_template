49b0ff67c0abd94f61f7246012060431b3434305
guillem roma <guillemroma.lopez@gmail.com>
2022-10-17 20:49:14 +0200

    update to rails 617

--- template.rb
+++ template.rb
@@ -1,13 +1,16 @@
 run "if uname | grep -q 'Darwin'; then pgrep spring | xargs kill -9; fi"
 
-# Gemfile
+# GEMFILE
 ########################################
-inject_into_file "Gemfile", before: "group :development, :test do" do
+inject_into_file 'Gemfile', before: 'group :development, :test do' do
   <<~RUBY
-    gem "devise"
-    gem "autoprefixer-rails"
-    gem "font-awesome-sass", "~> 6.1"
-    gem "simple_form", github: "heartcombo/simple_form"
+    gem 'devise'
+
+    gem 'autoprefixer-rails', '10.2.5'
+    gem 'font-awesome-sass', '~> 5.6.1'
+    gem 'simple_form', github: 'heartcombo/simple_form'
+
+    gem 'faker', :git => 'https://github.com/faker-ruby/faker.git', :branch => 'master'
     gem "pundit"
     gem 'whenever', require: false
     gem 'pry', '~> 0.13.1'
@@ -16,81 +19,83 @@ inject_into_file "Gemfile", before: "group :development, :test do" do
   RUBY
 end
 
-inject_into_file "Gemfile", after: 'gem "debug", platforms: %i[ mri mingw x64_mingw ]' do
-<<-RUBY
-
-  gem "dotenv-rails"
+inject_into_file 'Gemfile', after: 'group :development, :test do' do
+  <<-RUBY
+  gem 'pry-byebug'
+  gem 'pry-rails'
+  gem 'dotenv-rails'
   gem 'rspec-rails'
-RUBY
+  RUBY
 end
 
-gsub_file("Gemfile", '# gem "sassc-rails"', 'gem "sassc-rails"')
+gsub_file('Gemfile', /# gem 'redis'/, "gem 'redis'")
 
 # Assets
 ########################################
 run "rm -rf app/assets/stylesheets"
 run "rm -rf vendor"
-run "curl -L https://github.com/lewagon/rails-stylesheets/archive/master.zip > stylesheets.zip"
-run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-stylesheets-master/README.md"
-run "mv app/assets/rails-stylesheets-master app/assets/stylesheets"
+run "curl -L https://github.com/lewagon/rails-stylesheets/archive/no-update.zip > stylesheets.zip"
+run "unzip stylesheets.zip -d app/assets && rm -f stylesheets.zip && rm -f app/assets/rails-stylesheets-no-update/README.md"
+run "mv app/assets/rails-stylesheets-no-update app/assets/stylesheets"
 
-inject_into_file "config/initializers/assets.rb", before: "# Precompile additional assets." do
-  <<~RUBY
-    Rails.application.config.assets.paths << Rails.root.join("node_modules")
-  RUBY
-end
+# Dev environment
+########################################
+gsub_file('config/environments/development.rb', /config\.assets\.debug.*/, 'config.assets.debug = false')
 
 # Layout
 ########################################
+if Rails.version < "6"
+  scripts = <<~HTML
+    <%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>
+        <%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>
+  HTML
+  gsub_file('app/views/layouts/application.html.erb', "<%= javascript_include_tag 'application', 'data-turbolinks-track': 'reload' %>", scripts)
+end
 
-gsub_file(
-  "app/views/layouts/application.html.erb",
-  '<meta name="viewport" content="width=device-width,initial-scale=1">',
-  '<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'
-)
+gsub_file('app/views/layouts/application.html.erb', "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload' %>", "<%= javascript_pack_tag 'application', 'data-turbolinks-track': 'reload', defer: true %>")
 
-# Flashes
+style = <<~HTML
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>
+HTML
+gsub_file('app/views/layouts/application.html.erb', "<%= stylesheet_link_tag 'application', media: 'all', 'data-turbolinks-track': 'reload' %>", style)
+
+# Navbar
 ########################################
-file "app/views/shared/_flashes.html.erb", <<~HTML
-  <div class="navbar navbar-expand-sm navbar-light navbar">
-    <div class="container-fluid">
-      <%= link_to root_path, class: "navbar-brand" do %>
-        <p><strong>[company]</strong></p>
+file 'app/views/shared/_navbar.html.erb', <<~HTML
+  <div class="navbar navbar-expand-sm navbar-light navbar-lewagon">
+    <%= link_to "#", class: "navbar-brand" do %>
+      <%= image_tag "https://raw.githubusercontent.com/lewagon/fullstack-images/master/uikit/logo.png" %>
+    <% end %>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <% if user_signed_in? %>
+        <%= image_tag "https://cdn.onlinewebfonts.com/svg/img_404623.png", class: "avatar dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false %>
+        <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+          <%= link_to "Log out", destroy_user_session_path, :method => :delete, class: "dropdown-item" %>
+        </div>
       <% end %>
-
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <% if user_signed_in? %>
-            <li class="nav-item dropdown">
-              <%= image_tag "https://cdn.onlinewebfonts.com/svg/img_404623.png", class: "avatar dropdown-toggle", id: "navbarDropdown", data: { bs_toggle: "dropdown" }, 'aria-haspopup': true, 'aria-expanded': false %>
-              <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                <%= link_to "Log out", destroy_user_session_path, :method => :delete, class: "dropdown-item" %>
-              </div>
-            </li>
-          <% end %>
-        </ul>
       </div>
-    </div>
   </div>
 HTML
 
-inject_into_file "app/views/layouts/application.html.erb", after: "<body>" do
-  <<~HTML
-    <%= render "shared/navbar" %>
+inject_into_file 'app/views/layouts/application.html.erb', after: '<body>' do
+  <<-HTML
+
+    <%= render 'shared/navbar' %>
   HTML
 end
 
 # README
 ########################################
-markdown_file_content = <<~MARKDOWN
-  Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+markdown_file_content = <<-MARKDOWN
+Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
 MARKDOWN
-file "README.md", markdown_file_content, force: true
+file 'README.md', markdown_file_content, force: true
 
 # Generators
 ########################################
@@ -105,22 +110,22 @@ RUBY
 environment generators
 
 ########################################
-# After bundle
+# AFTER BUNDLE
 ########################################
 after_bundle do
   # Generators: db + simple form + pages controller
   ########################################
-  rails_command "db:drop db:create db:migrate"
-  generate("simple_form:install", "--bootstrap")
-  generate(:controller, "pages", "home", "--skip-routes", "--no-test-framework")
+  rails_command 'db:drop db:create db:migrate'
+  generate('simple_form:install', '--bootstrap')
+  generate(:controller, 'pages', 'home', '--skip-routes', '--no-test-framework')
 
   # Routes
   ########################################
-  route 'root to: "pages#home"'
+  route "root to: 'pages#home'"
 
-  # Gitignore
+  # Git ignore
   ########################################
-  append_file ".gitignore", <<~TXT
+  append_file '.gitignore', <<~TXT
     # Ignore .env file containing credentials.
     .env*
     # Ignore Mac and Linux file system files
@@ -130,47 +135,27 @@ after_bundle do
 
   # Devise install + user
   ########################################
-  generate("devise:install")
-  generate("devise", "User")
+  generate('devise:install')
+  generate('devise', 'User')
 
-  # Application controller
+  # App controller
   ########################################
-  run "rm app/controllers/application_controller.rb"
-  file "app/controllers/application_controller.rb", <<~RUBY
+  run 'rm app/controllers/application_controller.rb'
+  file 'app/controllers/application_controller.rb', <<~RUBY
     class ApplicationController < ActionController::Base
-      before_action :authenticate_user!
+    #{  "protect_from_forgery with: :exception\n" if Rails.version < "5.2"}  before_action :authenticate_user!
     end
   RUBY
 
   # migrate + devise views
   ########################################
-  rails_command "db:migrate"
-  generate("devise:views")
-  gsub_file(
-    "app/views/devise/registrations/new.html.erb",
-    "<%= simple_form_for(resource, as: resource_name, url: registration_path(resource_name)) do |f| %>",
-    "<%= simple_form_for(resource, as: resource_name, url: registration_path(resource_name), data: { turbo: :false }) do |f| %>"
-  )
-  gsub_file(
-    "app/views/devise/sessions/new.html.erb",
-    "<%= simple_form_for(resource, as: resource_name, url: session_path(resource_name)) do |f| %>",
-    "<%= simple_form_for(resource, as: resource_name, url: session_path(resource_name), data: { turbo: :false }) do |f| %>"
-  )
-  link_to = <<~HTML
-    <p>Unhappy? <%= link_to "Cancel my account", registration_path(resource_name), data: { confirm: "Are you sure?" }, method: :delete %></p>
-  HTML
-  button_to = <<~HTML
-    <div class="d-flex align-items-center">
-      <div>Unhappy?</div>
-      <%= button_to "Cancel my account", registration_path(resource_name), data: { confirm: "Are you sure?" }, method: :delete, class: "btn btn-link" %>
-    </div>
-  HTML
-  gsub_file("app/views/devise/registrations/edit.html.erb", link_to, button_to)
+  rails_command 'db:migrate'
+  generate('devise:views')
 
   # Pages Controller
   ########################################
-  run "rm app/controllers/pages_controller.rb"
-  file "app/controllers/pages_controller.rb", <<~RUBY
+  run 'rm app/controllers/pages_controller.rb'
+  file 'app/controllers/pages_controller.rb', <<~RUBY
     class PagesController < ApplicationController
       skip_before_action :authenticate_user!, only: [ :home ]
 
@@ -181,31 +166,34 @@ after_bundle do
 
   # Environments
   ########################################
-  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: "development"
-  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: "production"
+  environment 'config.action_mailer.default_url_options = { host: "http://localhost:3000" }', env: 'development'
+  environment 'config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }', env: 'production'
 
-  # Yarn
+  # Webpacker / Yarn
   ########################################
-  run "yarn add bootstrap @popperjs/core"
-  append_file "app/javascript/application.js", <<~JS
+  run 'yarn add bootstrap @popperjs/core'
+  run "rails webpacker:install:stimulus"
+  append_file 'app/javascript/packs/application.js', <<~JS
     import "bootstrap"
   JS
 
-  # Heroku
-  ########################################
-  run "bundle lock --add-platform x86_64-linux"
+  inject_into_file 'config/webpack/environment.js', before: 'module.exports' do
+    <<~JS
+      // Preventing Babel from transpiling NodeModules packages
+      environment.loaders.delete('nodeModules');
+    JS
+  end
 
   # Dotenv
   ########################################
-  run "touch '.env'"
+  run 'touch .env'
 
   # Rubocop
   ########################################
-  run "curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml"
+  run 'curl -L https://raw.githubusercontent.com/lewagon/rails-templates/master/.rubocop.yml > .rubocop.yml'
 
   # Git
   ########################################
-  git :init
-  git add: "."
+  git add: '.'
   git commit: "-m 'Initial commit with devise template from https://github.com/lewagon/rails-templates'"
 end
